a87af762de2794524c16c7866398512c002e9478
Audun Hilden <audunhilden@live.no>
2020-12-03 15:42:43 +0100

    Update audn1.js

--- pages/audn1.js
+++ pages/audn1.js
@@ -1,1 +1,20 @@
-https://www.mc-market.org/threads/618567/#post-4633204
+import React, { useState, useEffect } from 'react';
+
+{
+  /*
+let output;
+got('https://api.minetools.eu/ping/skyblock.minecraft.gg/8141').then((res) => {
+  output = JSON.parse(res).body;
+});
+*/
+}
+
+function Index() {
+  return (
+    <div className="container mx-auto ">
+    https://www.mc-market.org/threads/618567/#post-4633204
+    </div>
+  );
+}
+
+export default Index;
