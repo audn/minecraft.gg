b8e3e4945700dfcefaf61e4005641e9715766ee6
audun <audunhilden@live.no>
2023-02-10 06:38:25 +0100

    dummy search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { NextSeo } from 'next-seo';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { SyntheticEvent, useState } from 'react';
 import { Button } from '../common/components/Buttons';
 import { Form } from '../common/components/Form';
 import { DefaultLayout } from '../common/layouts/Default';
 
 export default function Home() {
   const [search, setSearch] = useState<string>('');
+  const router = useRouter();
+  function onSearch(e: SyntheticEvent) {
+    e.preventDefault();
+    router.push({ pathname: `/user/${search}` }, undefined, { shallow: true });
+  }
   return (
     <DefaultLayout>
       <NextSeo />
@@ -15,13 +21,23 @@ export default function Home() {
             <h1 className="font-ten text-white text-3xl">
               Minecraft User Lookup
             </h1>
-            <Form.Input
-              id="username"
-              placeholder="Username..."
-              value={search}
-              onChange={setSearch}
-            />
-            <Button.Primary title="GO!" className="text-xl" />
+            <Form.Layout
+              onSubmit={onSearch}
+              className="flex justify-center flex-col items-center space-y-10"
+            >
+              <Form.Input
+                id="username"
+                placeholder="Username..."
+                value={search}
+                onChange={setSearch}
+              />
+
+              <Button.Primary
+                title="GO!"
+                className="text-xl"
+                onClick={onSearch}
+              />
+            </Form.Layout>
           </div>
         </div>
       </div>
